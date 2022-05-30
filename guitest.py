d2850253d94d3e4308c4582e83d54e8dde148807
ishihara-su <ishihara.susumu@shizuoka.ac.jp>
2022-05-30 11:24:16 +0900

    Implemented draft of QAgent. Some bugs exist.

---
+++ guitest.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+
+"""
+    Test application to show and control the game board.
+"""
+
+BOARD_SIZE = 8
+
+layout = [
+    [sg.Text('Reversi')],
+    [sg.Graph((800, 800), (0, 450), (450, 0), key='-BOARD-',
+        change_submits=True, drag_submits=False)],
+    [sg.Button('Next'), sg.Button('Exit')]
+]
+
+window = sg.Window('Reversi', layout, finalize=True)
+g = window['-BOARD-']
+
+for row in range(BOARD_SIZE):
+    for col in range(BOARD_SIZE):
